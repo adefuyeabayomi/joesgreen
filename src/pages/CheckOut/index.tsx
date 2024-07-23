a7d21b0691bdb53074f388bf23f3f53f6e1af3a9
abayomi <adefuyeabayomi16@gmail.com>
2024-07-23 08:58:50 +0100

    update

--- src/pages/CheckOut/index.tsx
+++ src/pages/CheckOut/index.tsx
@@ -1,6 +1,10 @@
 import React,{useState} from "react";
 import { OrderCheckOutItem } from "../../components/orderItem";
 import './style.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { InputMain } from "../../components/input";
+
 
 export default function CheckOut (): React.JSX.Element {
     return (
@@ -9,7 +13,7 @@ export default function CheckOut (): React.JSX.Element {
                 <div className="py-3" />
                 <div className="container-fluid no-space">
                     <div className="row no-space">
-                        <div className="col-12 col-md-6 col-xl-9 no-space">
+                        <div className="col-12 no-space">
                             <div>
                                 <p className="font-heading-6 green-color-main font-medium">PROCEED TO CHECKOUT</p>
                                 <p className="font-p col-12 col-md-9">Check out your cart items. You can edit your meals with our addons. Just click the edit button.</p>
@@ -35,39 +39,16 @@ export default function CheckOut (): React.JSX.Element {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-12 col-md-6 col-xl-3 no-space">
+                        <div className="col-12 no-space">
                             <div className="orderContainer">
                                 <div>
-                                    <p className="font-subtitle">Order Summary</p>
+                                    <p className="font-heading-6 font-regular">Order Summary</p>
                                 </div>
                                 <div className="summaryItem">
-                                    <div className="container-fluid no-space">
-                                        <div className="row no-space">
-                                            <div className="col no-space">
-                                                <div>
-                                                    <div>
-                                                        <p className="font-p font-medium no-space">White Rice and Stew</p>
-                                                    </div>
-                                                    <div>
-                                                        <p className="font-p font-medium no-space">12 Plates</p>
-                                                    </div>
-                                                    <div>
-                                                        <p className="font-p font-regular no-space">Addons Included: Plantain, Coleslaw, Moi Moi, Gizzard</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div className="w-max-content">
-                                                <div>
-                                                    <p className="font-subtitle">N 3500</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="py-2" />
                                     <div className="order-summary-divider" />
                                     <div className="py-2" />
                                     <div className="container-fluid no-space">
-                                        <div className="row no-space">
+                                        <div className="row no-space font-p font-medium">
                                             <div className="col no-space">
                                                 <div>
                                                     <p>Grand Total</p>
@@ -79,7 +60,7 @@ export default function CheckOut (): React.JSX.Element {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="row no-space">
+                                        <div className="row no-space font-p font-medium">
                                             <div className="col no-space">
                                                 <div>
                                                     <p>Delivery Fees</p>
@@ -92,15 +73,22 @@ export default function CheckOut (): React.JSX.Element {
                                             </div>
                                         </div>
                                     </div>
+                                    <div className="py-2" />
                                     <div className="deliveryInformation">
                                         <div className="">
-                                            <p>Delivery Information</p>
+                                            <p className="font-subtitle font-medium">Delivery Information</p>
                                         </div>
+                                        <div className="py-1" />
                                         <div className="">
-                                            <input type="text" />
+                                            <InputMain value="" onChange={(e)=>{}} placeholder="Phone Number" icon={<FontAwesomeIcon icon={faEnvelope} />} />
                                         </div>
+                                        <div className="py-1" />
                                         <div className="">
-                                            <input type="text" />
+                                        <InputMain value="" onChange={(e)=>{}} placeholder="Delivery Information" icon={<FontAwesomeIcon icon={faEnvelope} />} />
+                                        </div>
+                                        <div className="py-2" />
+                                        <div>
+                                            <button className="checkout-btn">Proceed To Checkout</button>
                                         </div>
                                     </div>
                                 </div>
--- src/pages/SupportPage/index.tsx
+++ src/pages/SupportPage/index.tsx
@@ -1,19 +1,88 @@
 import React,{useState} from "react";
 
+import call from '../../assets/image16.png'
+import message from '../../assets/image15.png'
+
 import './style.css'
+import { InputMain } from "../../components/input";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 export default function SupportPage (): React.JSX.Element {
     return (
         <div>
             <div className="mainSpacing">
                 <div className="py-3" />
-                <div className="container-fluid">
-                    <div className="row no-space">
-                        <div className="loginFormContainer col-12 no-space">
-
+                <div className="support-page-container">
+                    <div className="util-header">
+                        <div>
+                            <p className="font-heading-h6 font-medium green-color-main text-center">Support Page</p>
                         </div>
-                        <div className="formSupport col-12 no-space">
+                        <div className="py-1" />
+                        <div className="util-divider" />
+                    </div>
+                    <div className="container-fluid no-space">
+                        <div className="row no-space align-items-center">
+                            <div className="col-12 col-md-6">
+                                <div className="contact-contents">
+                                    <div>
+                                        <p className="font-subtitle font-medium deep-green">Contact Us</p>
+                                    </div>
+                                    <div className="container-fluid no-space">
+                                        <div className="row no-space align-items-center">
+                                            <div className="w-max-content no-space">
+                                                <div className="SIImageContainer">
+                                                    <img src={message} />
+                                                </div>
+                                            </div>
+                                            <div className="col-6 no-space">
+                                                <div className="px-1">
+                                                    <p className="font-subtitle font-medium no-space">Email</p>
+                                                </div>
+                                                <div className="px-1">
+                                                    <p className="font-p font-medium no-space">support@joegreencafe.com</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="container-fluid no-space">
+                                        <div className="row no-space align-items-center">
+                                            <div className="w-max-content no-space">
+                                                <div className="SIImageContainer">
+                                                    <img src={call} />
+                                                </div>
+                                            </div>
+                                            <div className="col-6 no-space">
+                                                <div className="px-1">
+                                                    <p className="font-subtitle font-medium no-space">Call</p>
+                                                </div>
+                                                <div className="px-1">
+                                                    <p className="font-p font-medium no-space">+234 90-8374-4758</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
 
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-6">
+                                <div className="supportFormContainer">
+                                    <div>
+                                        <p className="font-subtitle font-medium">Send An Instant Message</p>
+                                    </div>
+                                    <div>
+                                        <p className="font-small font-regular">Your Email</p>
+                                        <InputMain icon={<FontAwesomeIcon icon={faShoppingCart}/>} value='' onChange={()=>{}} />
+                                    </div>
+                                    
+                                    <div>
+                                        <p className="font-small font-regular">Your Email</p>
+                                        <textarea>
+                                            
+                                        </textarea>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
